7cd465418e0d90550b34312c751b5ec2772baca4
Jose Bollo <jose.bollo@iot.bzh>
2020-11-25 10:18:43 +0100

    afb-extend: Add standard extending feature

---
+++ src/libafb/extend/afb-extend.c
@@ -0,0 +1,328 @@
+/*
+ * Copyright (C) 2015-2020 IoT.bzh Company
+ * Author: José Bollo <jose.bollo@iot.bzh>
+ *
+ * $RP_BEGIN_LICENSE$
+ * Commercial License Usage
+ *  Licensees holding valid commercial IoT.bzh licenses may use this file in
+ *  accordance with the commercial license agreement provided with the
+ *  Software or, alternatively, in accordance with the terms contained in
+ *  a written agreement between you and The IoT.bzh Company. For licensing terms
+ *  and conditions see https://www.iot.bzh/terms-conditions. For further
+ *  information use the contact form at https://www.iot.bzh/contact.
+ *
+ * GNU General Public License Usage
+ *  Alternatively, this file may be used under the terms of the GNU General
+ *  Public license version 3. This license is as published by the Free Software
+ *  Foundation and appearing in the file LICENSE.GPLv3 included in the packaging
+ *  of this file. Please review the following information to ensure the GNU
+ *  General Public License requirements will be met
+ *  https://www.gnu.org/licenses/gpl-3.0.html.
+ * $RP_END_LICENSE$
+ */
+
+#include "libafb-config.h"
+
+#if WITH_EXTENSION
+
+#include <string.h>
+
+#include <json-c/json.h>
+
+#include "afb-extension.h"
+#include "afb-extend.h"
+
+#include "sys/verbose.h"
+#include "sys/x-dynlib.h"
+#include "utils/wrap-json.h"
+
+#if WITH_DIRENT
+#include "utils/path-search.h"
+#endif
+
+#define MANIFEST	"AfbExtensionManifest"
+#define OPTIONS_V1	"AfbExtensionOptionsV1"
+#define CONFIG_V1	"AfbExtensionConfigV1"
+#define DECLARE_V1	"AfbExtensionDeclareV1"
+#define SERVE_V1	"AfbExtensionServeV1"
+#define EXIT_V1		"AfbExtensionExitV1"
+
+struct extension
+{
+	struct extension *next;
+	struct afb_extension_manifest *manifest;
+	x_dynlib_t handle;
+	void *data;
+	char path[];
+};
+
+static struct extension *extensions;
+
+static int load_extension(const char *path)
+{
+	struct afb_extension_manifest *manifest;
+	struct extension *ext;
+	x_dynlib_t handle;
+	int rc;
+
+	/* try to load */
+	DEBUG("Trying extension %s", path);
+	rc = x_dynlib_open(path, &handle, 1, 0);
+	if (rc < 0) {
+		DEBUG("can't load extension %s", path);
+		return 0;
+	}
+
+	/* search the symbol */
+	rc = x_dynlib_symbol(&handle, MANIFEST, (void**)&manifest);
+	if (rc < 0) {
+		DEBUG("Not an extension %s", path);
+		rc = 0;
+	} else if (!manifest || manifest->magic != AFB_EXTENSION_MAGIC
+			|| !manifest->name) {
+		ERROR("Manifest error of extension %s", path);
+		rc = X_EINVAL;
+	} else if (manifest->version != 1) {
+		ERROR("Unsupported version %d of extension %s", manifest->version, path);
+		rc = X_ENOTSUP;
+	} else {
+		ext = malloc(strlen(path) + 1 + sizeof *ext);
+		if (!ext)
+			rc = X_ENOMEM;
+		else {
+			NOTICE("Adding extension %s of %s", manifest->name, path);
+			ext->next = extensions;
+			ext->handle = handle;
+			ext->manifest = manifest;
+			ext->data = 0;
+			strcpy(ext->path, path);
+			extensions = ext;
+			return 1;
+		}
+	}
+	x_dynlib_close(&handle);
+	return rc;
+}
+
+static void load_extension_cb(void *closure, struct json_object *value)
+{
+	int rc, *ret = closure;
+
+	if (*ret >= 0 && json_object_is_type(value, json_type_string)) {
+		rc = load_extension(json_object_get_string(value));
+		if (rc < *ret)
+			*ret = rc;
+	}
+}
+
+#if WITH_DIRENT
+/**
+ * callback of files
+ */
+static int try_extension(void *closure, struct path_search_item *item)
+{
+	static char extension[] = ".so";
+	int rc, *ret = closure;
+
+	/* only try files having ".so" extension */
+	if (item->namelen < (sizeof extension - 1)
+	 || memcmp(&item->name[item->namelen - (sizeof extension - 1)], extension, sizeof extension))
+		return 0;
+
+	/* try to get it as a binding */
+	rc = load_extension(item->path);
+	if (rc >= 0)
+		return 0; /* got it */
+
+	/* report the error and tell to stop exploration of files */
+	*ret = rc;
+	return 1;
+}
+
+/**
+ * function to filter out the directories that must not be entered
+ */
+static int filterdirs(void *closure, struct path_search_item *item)
+{
+	int result = item->name[0] != '.';
+	if (result)
+		INFO("Scanning dir=[%s] for extensions", item->path);
+	return result;
+}
+
+static int load_extpath(const char *value)
+{
+	int rc;
+	struct path_search *ps;
+
+	rc = path_search_make_dirs(&ps, value);
+	if (rc >= 0) {
+		path_search_filter(ps, PATH_SEARCH_FILE|PATH_SEARCH_RECURSIVE|PATH_SEARCH_FLEXIBLE,
+			try_extension, &rc, filterdirs);
+		path_search_unref(ps);
+	}
+	return rc;
+}
+
+static void load_extpath_cb(void *closure, struct json_object *value)
+{
+	int *ret = closure;
+	int rc;
+
+	if (*ret >= 0 && json_object_is_type(value, json_type_string)) {
+		rc = load_extpath(json_object_get_string(value));
+		if (rc < *ret)
+			*ret = rc;
+	}
+}
+
+#endif
+
+int afb_extend_load(struct json_object *config)
+{
+	struct extension *ext, *head;
+	struct json_object *array;
+	int rc;
+
+	/* load extensions */
+	rc = 1;
+	if (json_object_object_get_ex(config, "extension", &array))
+		wrap_json_optarray_for_all(array, load_extension_cb, &rc);
+#if WITH_DIRENT
+	/* search extensions */
+	if (rc >= 0 && json_object_object_get_ex(config, "extpaths", &array))
+		wrap_json_optarray_for_all(array, load_extpath_cb, &rc);
+#endif
+
+	/* revert list of extensions */
+	head = 0;
+	while ((ext = extensions)) {
+		extensions = ext->next;
+		ext->next = head;
+		head = ext;
+	}
+	extensions = head;
+
+	return rc;
+}
+
+int afb_extend_get_options(const struct argp_option ***options_array_result, const char ***names)
+{
+	int rc, n, s;
+	struct extension *ext;
+	const struct argp_option *options;
+	const struct argp_option **oar;
+	const char **enam;
+
+	/* allocates enough for the result */
+	for (n = 0, ext = extensions ; ext ; ext = ext->next, n++);
+	*options_array_result = oar = malloc(n * sizeof *oar);
+	*names = enam = malloc(n * sizeof *enam);
+	if (!oar || !enam) {
+		if (oar) {
+			free(oar);
+			*options_array_result = 0;
+		}
+		if (enam) {
+			free(enam);
+			*names = 0;
+		}
+		rc = X_ENOMEM;
+	}
+	else {
+		/* initialize the results */
+		n = 0;
+		for (ext = extensions ; ext ; ext = ext->next) {
+			s = x_dynlib_symbol(&ext->handle, OPTIONS_V1, (void**)&options);
+			if (s >= 0) {
+				oar[n] = options;
+				enam[n] = ext->manifest->name;
+				n++;
+			}
+		}
+		rc = n;
+	}
+	return rc;
+}
+
+int afb_extend_config(struct json_object *config)
+{
+	int rc, s;
+	struct extension *ext;
+	int (*config_v1)(void **data, struct json_object *config);
+	struct json_object *root, *obj;
+
+	root = 0;
+	json_object_object_get_ex(config, "@extconfig", &root);
+	rc = 0;
+	for (ext = extensions ; ext ; ext = ext->next) {
+		s = x_dynlib_symbol(&ext->handle, CONFIG_V1, (void**)&config_v1);
+		if (s >= 0) {
+			obj = 0;
+			json_object_object_get_ex(root, ext->manifest->name, &obj);
+			s = config_v1(&ext->data, obj);
+			if (s < 0)
+				rc = s;
+		}
+	}
+	return rc;
+}
+
+int afb_extend_declare(struct afb_apiset *declare_set, struct afb_apiset *call_set)
+{
+	int rc, s;
+	struct extension *ext;
+	int (*declare_v1)(void *data, struct afb_apiset *declare_set, struct afb_apiset *call_set);
+
+	rc = 0;
+	for (ext = extensions ; ext ; ext = ext->next) {
+		s = x_dynlib_symbol(&ext->handle, DECLARE_V1, (void**)&declare_v1);
+		if (s >= 0) {
+			s = declare_v1(ext->data, declare_set, call_set);
+			if (s < 0)
+				rc = s;
+		}
+	}
+	return rc;
+}
+
+int afb_extend_serve(struct afb_apiset *call_set)
+{
+	int rc, s;
+	struct extension *ext;
+	int (*serve_v1)(void *data, struct afb_apiset *call_set);
+
+	rc = 0;
+	for (ext = extensions ; ext ; ext = ext->next) {
+		s = x_dynlib_symbol(&ext->handle, SERVE_V1, (void**)&serve_v1);
+		if (s >= 0) {
+			s = serve_v1(ext->data, call_set);
+			if (s < 0)
+				rc = s;
+		}
+	}
+	return rc;
+}
+
+int afb_extend_exit(struct afb_apiset *declare_set)
+{
+	int rc, s;
+	struct extension *ext;
+	int (*exit_v1)(void *data, struct afb_apiset *declare_set);
+
+	rc = 0;
+	while ((ext = extensions)) {
+		extensions = ext->next;
+		s = x_dynlib_symbol(&ext->handle, EXIT_V1, (void**)&exit_v1);
+		if (s >= 0) {
+			s = exit_v1(ext->data, declare_set);
+			if (s < 0)
+				rc = s;
+		}
+		x_dynlib_close(&ext->handle);
+		free(ext);
+	}
+	return rc;
+}
+
+#endif
---
+++ src/libafb/extend/afb-extension.h
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2015-2020 IoT.bzh Company
+ * Author: José Bollo <jose.bollo@iot.bzh>
+ *
+ * $RP_BEGIN_LICENSE$
+ * Commercial License Usage
+ *  Licensees holding valid commercial IoT.bzh licenses may use this file in
+ *  accordance with the commercial license agreement provided with the
+ *  Software or, alternatively, in accordance with the terms contained in
+ *  a written agreement between you and The IoT.bzh Company. For licensing terms
+ *  and conditions see https://www.iot.bzh/terms-conditions. For further
+ *  information use the contact form at https://www.iot.bzh/contact.
+ *
+ * GNU General Public License Usage
+ *  Alternatively, this file may be used under the terms of the GNU General
+ *  Public license version 3. This license is as published by the Free Software
+ *  Foundation and appearing in the file LICENSE.GPLv3 included in the packaging
+ *  of this file. Please review the following information to ensure the GNU
+ *  General Public License requirements will be met
+ *  https://www.gnu.org/licenses/gpl-3.0.html.
+ * $RP_END_LICENSE$
+ */
+
+#pragma once
+
+#define AFB_EXTENSION_MAGIC    78612
+#define AFB_EXTENSION_VERSION  1
+
+struct afb_extension_manifest
+{
+	unsigned magic;
+	unsigned version;
+	const char *name;
+};
+
+#define AFB_EXTENSION(somename) \
+	struct afb_extension_manifest AfbExtensionManifest = { \
+		.magic = AFB_EXTENSION_MAGIC, \
+		.version = AFB_EXTENSION_VERSION, \
+		.name = #somename \
+	};
+
+struct json_object;
+struct afb_apiset;
+
+#include <argp.h>
+
+extern const struct argp_option AfbExtensionOptionsV1[];
+
+extern int AfbExtensionConfigV1(void **data, struct json_object *config);
+extern int AfbExtensionDeclareV1(void *data, struct afb_apiset *declare_set, struct afb_apiset *call_set);
+extern int AfbExtensionServeV1(void *data, struct afb_apiset *call_set);
+extern int AfbExtensionExitV1(void *data, struct afb_apiset *declare_set);
