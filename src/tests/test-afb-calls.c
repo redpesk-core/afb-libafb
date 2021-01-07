fde36e7e6cb0d6db6cb5bd0285a7977f5ced23e1
Johann Gautier <johann.gautier@iot.bzh>
2021-01-07 15:06:50 +0100

    test-afb-calls: Improve test of calls and subcalls

--- src/tests/test-afb-calls.c
+++ src/tests/test-afb-calls.c
@@ -1,3 +1,29 @@
+/*
+ Copyright (C) 2015-2021 IoT.bzh Company
+
+ Author: Johann Gautier <johann.gautier@iot.bzh>
+
+ $RP_BEGIN_LICENSE$
+ Commercial License Usage
+  Licensees holding valid commercial IoT.bzh licenses may use this file in
+  accordance with the commercial license agreement provided with the
+  Software or, alternatively, in accordance with the terms contained in
+  a written agreement between you and The IoT.bzh Company. For licensing terms
+  and conditions see https://www.iot.bzh/terms-conditions. For further
+  information use the contact form at https://www.iot.bzh/contact.
+
+ GNU General Public License Usage
+  Alternatively, this file may be used under the terms of the GNU General
+  Public license version 3. This license is as published by the Free Software
+  Foundation and appearing in the file LICENSE.GPLv3 included in the packaging
+  of this file. Please review the following information to ensure the GNU
+  General Public License requirements will be met
+  https://www.gnu.org/licenses/gpl-3.0.html.
+ $RP_END_LICENSE$
+*/
+
+#include "libafb-config.h"
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -5,25 +31,29 @@
 
 #include <check.h>
 
-#include <afb/afb-binding-v4.h>
-
-#include "libafb-config.h"
+#include <afb/afb-req-subcall-flags.h>
 
 #include "utils/wrap-json.h"
 #include "apis/afb-api-so.h"
 #include "core/afb-calls.h"
 #include "core/afb-api-common.h"
+#include "core/afb-req-common.h"
 #include "core/afb-apiset.h"
 #include "core/afb-jobs.h"
+#include "core/afb-sched.h"
 #include "core/afb-data.h"
 #include "core/afb-type.h"
+#include "core/afb-evt.h"
+#include "core/afb-type-predefined.h"
+#include "core/afb-params.h"
+
 
 
 #define PATH_BUF_SIZE 200
 #define NBPARAMS 3
 
 #if WITH_REQ_PROCESS_ASYNC
-#define RUNJOB while(afb_jobs_get_pending_count()) { afb_jobs_run(afb_jobs_dequeue(0)); }
+#define RUNJOB afb_sched_wait_idle(1,1);
 #else
 #define RUNJOB (void)0
 #endif
@@ -32,6 +62,52 @@
 #define p2i(x)  ((int)((intptr_t)(x)))
 
 /*********************************************************************/
+struct afb_req_common *test_reply_req;
+int test_reply_status;
+unsigned test_reply_nreplies;
+struct afb_data *const *test_reply_replies;
+void test_reply(struct afb_req_common *req, int status, unsigned nreplies, struct afb_data *const replies[])
+{
+	test_reply_req = req;
+	test_reply_status = status;
+	test_reply_nreplies = nreplies;
+	test_reply_replies = replies;
+}
+
+struct afb_req_common *test_unref_req;
+void test_unref(struct afb_req_common *req)
+{
+	test_unref_req = req;
+	afb_req_common_cleanup(req);
+}
+
+struct afb_req_common *test_subscribe_req;
+struct afb_evt *test_subscribe_event;
+int test_subscribe(struct afb_req_common *req, struct afb_evt *event)
+{
+	test_subscribe_req = req;
+	test_subscribe_event = event;
+	return 0;
+}
+
+struct afb_req_common *test_unsubscribe_req;
+struct afb_evt *test_unsubscribe_event;
+int test_unsubscribe(struct afb_req_common *req, struct afb_evt *event)
+{
+	test_unsubscribe_req = req;
+	test_unsubscribe_event = event;
+	return 0;
+}
+
+struct afb_req_common_query_itf test_queryitf =
+{
+	.reply = test_reply,
+	.unref = test_unref,
+	.subscribe = test_subscribe,
+	.unsubscribe = test_unsubscribe
+};
+
+/**************************************************************/
 
 int gval, dataClosureGval, verbDataGval ;
 
@@ -61,17 +137,8 @@ int getpath(char buffer[PATH_BUF_SIZE], const char *base, int ival)
 	if (rc == 0)
 		fprintf(stderr, "FOUND %s for %s/%d\n", buffer, base, ival);
 	else
-		fprintf(stderr, "Cna't find file %s/%d\n", base, ival);
+		fprintf(stderr, "Can't find file %s/%d\n", base, ival);
 	return rc;
-}
-
-void show(struct afb_api_common * comapi){
-    fprintf(stderr, "comapi.refcount = %d\n", comapi->refcount);
-    fprintf(stderr, "comapi.state = %d\n", comapi->state);
-    fprintf(stderr, "comapi.settings = %s\n", json_object_to_json_string(comapi->settings));
-	fprintf(stderr, "comapi.path = %s\n", comapi->path);
-	fprintf(stderr, "comapi.name = %s\n", comapi->name);
-	fprintf(stderr, "comapi.sealed = %d\n", comapi->sealed);
 }
 
 void dataClosureCB(void *arg)
@@ -88,22 +155,22 @@ void testCB(void *closure1, void *closure2, void *closure3, int a, unsigned int 
 	}
 }
 
-/*********************************************************************/
-/* Test ... */
 START_TEST (test)
 {
 	struct afb_data * params[NBPARAMS];
+	struct afb_data * replies[NBPARAMS+1];
 	struct afb_type * type1;
     struct afb_api_common comapi;
+	struct afb_req_common req;
 	struct afb_apiset * declare_set;
     struct afb_apiset * call_set;
     char name[] = "hello";
     char info[] = "Info";
     char path[PATH_BUF_SIZE];
+	unsigned int nreplies;
+	int status;
 
 	int rc, i, checksum;
-
-	fprintf(stderr, "### test sync call");
 
 	// preparing params
 	type1 = afb_type_get("type1");
@@ -131,95 +198,211 @@ START_TEST (test)
     // inti it's api
     afb_api_common_init(&comapi, declare_set, call_set, name, 0, info, 0, path, 0);
 
-    // call one of it's verb
-    afb_calls_call(&comapi, name, "hello", NBPARAMS, params, testCB, NULL, NULL, NULL);
+	// inti a common req
+	afb_req_common_init(&req, &test_queryitf, "toto","patatate", 0, NULL);
 
-	dataClosureGval = verbDataGval = 0;
+	
+	/***** Test acync calls *****/
+	fprintf(stderr, "\n### Test async calls\n");
 
-    RUNJOB // if the system is not set to run jobs automaticaly run the jobs
-
-    show(&comapi);
-
+	// Test req_calls_call
+	verbDataGval = dataClosureGval = 0;
+	afb_params_addref(NBPARAMS, params); // increase params referensing to reuse it after
+	afb_calls_call(&comapi, name, "call", NBPARAMS, params, testCB, NULL, NULL, NULL);
+	RUNJOB // if the system is not set to run jobs automaticaly run the jobs
 	fprintf(stderr, "dataClosureGval = %d\n", dataClosureGval);
 	fprintf(stderr, "verbDataGval = %d\n", verbDataGval);
-
-    // check that the testCB receved correctly the verb reply
 	ck_assert_int_eq(verbDataGval, checksum);
+	ck_assert_int_eq(dataClosureGval, 0);
 
-	// check that data went through closure
-	ck_assert_int_eq(dataClosureGval, checksum);
-
-	ck_assert_int_eq(comapi.refcount, 1);
-	ck_assert_int_eq(comapi.state, 0);
-}
-END_TEST
-
-START_TEST (test_sync_call){
-
-	struct afb_data * params[NBPARAMS];
-	struct afb_data * replies[NBPARAMS];
-	struct afb_type * type1;
-    struct afb_api_common comapi;
-	struct afb_apiset * declare_set;
-    struct afb_apiset * call_set;
-    char name[] = "hello";
-    char info[] = "Info";
-    char path[PATH_BUF_SIZE];
-	unsigned nreplies;
-	int rc, i, checksum;
-
-	fprintf(stderr, "### test sync call");
-
-	// preparing params
-	type1 = afb_type_get("type1");
-	if (!type1) {
-		rc = afb_type_register(&type1, "type1", 0, 0, 0);
-		fprintf(stderr, "afb_type_register returned : %d\n", rc);
-		ck_assert_int_eq(rc, 0);
-	}
-	checksum = 0;
-	for(i=1; i<=NBPARAMS;  i++){
-		fprintf(stderr, "creating data with closure = %d\n", i);
-		rc = afb_data_create_raw(&params[i-1], type1, i2p(i), 0, dataClosureCB, i2p(i));
-		checksum += i;
-	}
-
-	declare_set = afb_apiset_create("toto", 1);
-	call_set = afb_apiset_create("tata", 1);
-
-	gval = 0;
-
-	// load a binding
-	getpath(path, "libhello.so", 0);
-    afb_api_so_add_binding(path, call_set, call_set);
-
-    // inti it's api
-    afb_api_common_init(&comapi, declare_set, call_set, name, 0, info, 0, path, 0);
-
-    // call one of it's verb
-    afb_calls_call_sync(&comapi, name, "hello", NBPARAMS, params, 0, &nreplies, replies);
-
-	dataClosureGval = verbDataGval = 0;
-
-    show(&comapi);
-
-	fprintf(stderr, "%d replies\n", nreplies);
-
-	for (i=0; i<nreplies; i++) {
-		ck_assert_int_eq(p2i(afb_data_const_pointer(replies[i])), i+1);
-	}
-
+	// Test req_calls_subcall
+	verbDataGval = dataClosureGval = 0;
+	afb_params_addref(NBPARAMS, params); // increase params referensing to reuse it after
+	afb_calls_subcall(&comapi, name, "call", NBPARAMS, params, testCB, NULL, NULL, NULL, &req, 0);
+	RUNJOB // if the system is not set to run jobs automaticaly run the jobs
 	fprintf(stderr, "dataClosureGval = %d\n", dataClosureGval);
+	fprintf(stderr, "verbDataGval = %d\n", verbDataGval);
+	ck_assert_int_eq(verbDataGval, checksum);
+	ck_assert_int_eq(dataClosureGval, 0);
 
-	// check that data went through closure
+	// Test req_calls_subscribe_cb
+	verbDataGval = dataClosureGval = 0;
+	afb_params_addref(NBPARAMS, params); // increase params referensing to reuse it after
+    afb_calls_call(&comapi, name, "subscribe", NBPARAMS, params, testCB, NULL, NULL, NULL);
+	RUNJOB // if the system is not set to run jobs automaticaly run the jobs
+	fprintf(stderr, "dataClosureGval = %d\n", dataClosureGval);
+	fprintf(stderr, "verbDataGval = %d\n", verbDataGval);
+	ck_assert_int_eq(verbDataGval, checksum);
+	ck_assert_int_eq(dataClosureGval, 0);
+
+	// Test req_calls_unsubscribe_cb
+	verbDataGval = dataClosureGval = 0;
+	afb_params_addref(NBPARAMS, params); // increase params referensing to reuse it after
+	afb_calls_call(&comapi, name, "unsubscribe", NBPARAMS, params, testCB, NULL, NULL, NULL);
+    RUNJOB // if the system is not set to run jobs automaticaly run the jobs
+	fprintf(stderr, "dataClosureGval = %d\n", dataClosureGval);
+	fprintf(stderr, "verbDataGval = %d\n", verbDataGval);
+	ck_assert_int_eq(verbDataGval, checksum);
+	ck_assert_int_eq(dataClosureGval, 0);
+
+	// test afb_calls_call_hooking
+	verbDataGval = dataClosureGval = 0;
+	afb_params_addref(NBPARAMS, params); // increase params referensing to reuse it after
+	afb_calls_call_hooking(&comapi, name, "call", NBPARAMS, params, testCB, NULL, NULL, NULL);
+	RUNJOB // if the system is not set to run jobs automaticaly run the jobs
+	fprintf(stderr, "dataClosureGval = %d\n", dataClosureGval);
+	fprintf(stderr, "verbDataGval = %d\n", verbDataGval);
+	ck_assert_int_eq(verbDataGval, checksum);
+	ck_assert_int_eq(dataClosureGval, 0);
+
+	// Test afb_calls_subcall_hooking
+	verbDataGval = dataClosureGval = 0;
+	afb_params_addref(NBPARAMS, params); // increase params referensing to reuse it after
+	afb_calls_subcall_hooking(&comapi, name, "call", NBPARAMS, params, testCB, NULL, NULL, NULL, &req, 0);
+	RUNJOB // if the system is not set to run jobs automaticaly run the jobs
+	fprintf(stderr, "dataClosureGval = %d\n", dataClosureGval);
+	fprintf(stderr, "verbDataGval = %d\n", verbDataGval);
+	ck_assert_int_eq(verbDataGval, checksum);
+	ck_assert_int_eq(dataClosureGval, 0);
+
+
+	/***** Test sync_calls *****/
+	fprintf(stderr, "\n### Test sync calls\n");
+
+	// test sync call
+	nreplies = NBPARAMS+1;
+	afb_params_addref(NBPARAMS, params); // increase params referensing to reuse it after
+	ck_assert_int_eq(0, afb_calls_call_sync(&comapi, name, "call", NBPARAMS, params, &status, &nreplies, replies));
+	fprintf(stderr, "nreplyes = %d\n", nreplies);
+	fprintf(stderr, "status = %d\n", status);
+	ck_assert_int_eq(status, 0);
+	ck_assert_int_eq(NBPARAMS, nreplies);
+	for (i=0; i<nreplies; i++) {
+		ck_assert_int_eq(i+1, p2i(afb_data_const_pointer(replies[i])));
+		ck_assert_ptr_eq(replies[i], params[i]);
+	}
+	afb_params_unref(nreplies, replies);
+
+	// test sync subscribe
+	nreplies = NBPARAMS+1;
+	afb_params_addref(NBPARAMS, params); // increase params referensing to reuse it after
+	ck_assert_int_eq(0, afb_calls_call_sync(&comapi, name, "subscribe", NBPARAMS, params, &status, &nreplies, replies));
+	fprintf(stderr, "nreplyes = %d\n", nreplies);
+	fprintf(stderr, "status = %d\n", status);
+	ck_assert_int_eq(status, 0);
+	ck_assert_int_eq(NBPARAMS, nreplies);
+	for (i=0; i<nreplies; i++) {
+		ck_assert_int_eq(i+1, p2i(afb_data_const_pointer(replies[i])));
+		ck_assert_ptr_eq(replies[i], params[i]);
+	}
+	afb_params_unref(nreplies, replies);
+
+	// test sync unsubscribe
+	nreplies = NBPARAMS+1;
+	afb_params_addref(NBPARAMS, params); // increase params referensing to reuse it after
+	ck_assert_int_eq(0, afb_calls_call_sync(&comapi, name, "unsubscribe", NBPARAMS, params, &status, &nreplies, replies));
+	fprintf(stderr, "nreplyes = %d\n", nreplies);
+	fprintf(stderr, "status = %d\n", status);
+	ck_assert_int_eq(NBPARAMS, nreplies);
+	ck_assert_int_eq(status, 0);
+	for (i=0; i<nreplies; i++) {
+		ck_assert_int_eq(i+1, p2i(afb_data_const_pointer(replies[i])));
+		ck_assert_ptr_eq(replies[i], params[i]);
+	}
+	afb_params_unref(nreplies, replies);
+
+	// test sync subcall
+	nreplies = NBPARAMS+1;
+	afb_params_addref(NBPARAMS, params); // increase params referensing to reuse it after
+	ck_assert_int_eq(0, afb_calls_subcall_sync(&comapi, name, "call", NBPARAMS, params, &status, &nreplies, replies, &req, afb_req_subcall_pass_events));
+	fprintf(stderr, "nreplyes = %d\n", nreplies);
+	fprintf(stderr, "status = %d\n", status);
+	ck_assert_int_eq(status, 0);
+	ck_assert_int_eq(NBPARAMS, nreplies);
+	for (i=0; i<nreplies; i++) {
+		ck_assert_int_eq(i+1, p2i(afb_data_const_pointer(replies[i])));
+		ck_assert_ptr_eq(replies[i], params[i]);
+	}
+	afb_params_unref(nreplies, replies);
+
+	// test sync subcall subscribe
+	nreplies = NBPARAMS+1;
+	afb_params_addref(NBPARAMS, params); // increase params referensing to reuse it after
+	ck_assert_int_eq(0, afb_calls_subcall_sync(&comapi, name, "subscribe", NBPARAMS, params, &status, &nreplies, replies, &req, afb_req_subcall_pass_events));
+	fprintf(stderr, "nreplyes = %d\n", nreplies);
+	fprintf(stderr, "status = %d\n", status);
+	ck_assert_int_eq(status, 0);
+	ck_assert_int_eq(NBPARAMS, nreplies);
+	for (i=0; i<nreplies; i++) {
+		ck_assert_int_eq(i+1, p2i(afb_data_const_pointer(replies[i])));
+		ck_assert_ptr_eq(replies[i], params[i]);
+	}
+	afb_params_unref(nreplies, replies);
+
+	// test sync subcall unsubscribe
+	nreplies = NBPARAMS+1;
+	afb_params_addref(NBPARAMS, params); // increase params referensing to reuse it after
+	ck_assert_int_eq(0, afb_calls_subcall_sync(&comapi, name, "unsubscribe", NBPARAMS, params, &status, &nreplies, replies, &req, afb_req_subcall_pass_events));
+	fprintf(stderr, "nreplyes = %d\n", nreplies);
+	fprintf(stderr, "status = %d\n", status);
+	ck_assert_int_eq(status, 0);
+	ck_assert_int_eq(NBPARAMS, nreplies);
+	for (i=0; i<nreplies; i++) {
+		ck_assert_int_eq(i+1, p2i(afb_data_const_pointer(replies[i])));
+		ck_assert_ptr_eq(replies[i], params[i]);
+	}
+	afb_params_unref(nreplies, replies);
+
+	// test sync call hooking
+	nreplies = NBPARAMS+1;
+	afb_params_addref(NBPARAMS, params); // increase params referensing to reuse it after
+	ck_assert_int_eq(0, afb_calls_call_sync_hooking(&comapi, name, "call", NBPARAMS, params, &status, &nreplies, replies));
+	fprintf(stderr, "nreplyes = %d\n", nreplies);
+	fprintf(stderr, "status = %d\n", status);
+	ck_assert_int_eq(status, 0);
+	ck_assert_int_eq(NBPARAMS, nreplies);
+	for (i=0; i<nreplies; i++) {
+		ck_assert_int_eq(i+1, p2i(afb_data_const_pointer(replies[i])));
+		ck_assert_ptr_eq(replies[i], params[i]);
+	}
+	afb_params_unref(nreplies, replies);
+
+	// test sync subcall hooking
+	nreplies = NBPARAMS+1;
+	afb_params_addref(NBPARAMS, params); // increase params referensing to reuse it after
+	ck_assert_int_eq(0, afb_calls_subcall_sync_hooking(&comapi, name, "call", NBPARAMS, params, &status, &nreplies, replies, &req, 0));
+	fprintf(stderr, "nreplyes = %d\n", nreplies);
+	fprintf(stderr, "status = %d\n", status);
+	ck_assert_int_eq(status, 0);
+	ck_assert_int_eq(NBPARAMS, nreplies);
+	for (i=0; i<nreplies; i++) {
+		ck_assert_int_eq(i+1, p2i(afb_data_const_pointer(replies[i])));
+		ck_assert_ptr_eq(replies[i], params[i]);
+	}
+	afb_params_unref(nreplies, replies);
+
+	// test the handling of a table of reply smaller than expected
+	nreplies = NBPARAMS-1;
+	afb_params_addref(NBPARAMS, params); // increase params referensing to reuse it after
+	ck_assert_int_eq(0, afb_calls_call_sync(&comapi, name, "call", NBPARAMS, params, &status, &nreplies, replies));
+	fprintf(stderr, "nreplyes = %d\n", nreplies);
+	fprintf(stderr, "status = %d\n", status);
+	ck_assert_int_eq(status, 0);
+	ck_assert_int_eq(NBPARAMS-1, nreplies);
+	for (i=0; i<nreplies; i++) {
+		ck_assert_int_eq(i+1, p2i(afb_data_const_pointer(replies[i])));
+		ck_assert_ptr_eq(replies[i], params[i]);
+	}
+	afb_params_unref(nreplies, replies);
+
+	afb_params_unref(NBPARAMS, params);
+
+	RUNJOB // if the system is not set to run jobs automaticaly run the jobs
+
 	ck_assert_int_eq(dataClosureGval, checksum);
 
-	ck_assert_int_eq(comapi.refcount, 1);
-	ck_assert_int_eq(comapi.state, 0);
 }
 END_TEST
-
-/*********************************************************************/
 
 static Suite *suite;
 static TCase *tcase;
@@ -242,6 +425,5 @@ int main(int ac, char **av)
 	mksuite("afb-calls");
 		addtcase("afb-calls");
 			addtest(test);
-			// addtest(test_sync_call);
 	return !!srun();
 }
--- src/tests/test-bindings/hello.c
+++ src/tests/test-bindings/hello.c
@@ -1,10 +1,10 @@
 /*
- Copyright (C) 2015-2021 IoT.bzh Company
+  Copyright (C) 2015-2021 IoT.bzh Company
 
- Author: José Bollo <jose.bollo@iot.bzh>
+  Author: José Bollo <jose.bollo@iot.bzh>
 
- $RP_BEGIN_LICENSE$
- Commercial License Usage
+  $RP_BEGIN_LICENSE$
+  Commercial License Usage
   Licensees holding valid commercial IoT.bzh licenses may use this file in
   accordance with the commercial license agreement provided with the
   Software or, alternatively, in accordance with the terms contained in
@@ -12,15 +12,16 @@
   and conditions see https://www.iot.bzh/terms-conditions. For further
   information use the contact form at https://www.iot.bzh/contact.
 
- GNU General Public License Usage
+  GNU General Public License Usage
   Alternatively, this file may be used under the terms of the GNU General
   Public license version 3. This license is as published by the Free Software
   Foundation and appearing in the file LICENSE.GPLv3 included in the packaging
   of this file. Please review the following information to ensure the GNU
   General Public License requirements will be met
   https://www.gnu.org/licenses/gpl-3.0.html.
- $RP_END_LICENSE$
+  $RP_END_LICENSE$
 */
+
 #include <stdio.h>
 
 #define AFB_BINDING_VERSION 4
@@ -30,22 +31,95 @@
 #define APINAME "hello"
 #endif
 
-static int mainctl(afb_api_x4_t api, afb_ctlid_t ctlid, afb_ctlarg_t ctlarg, void *userdata){
-    return 0;
-}
+afb_event_t event;
+
 static void hello(afb_req_t request, unsigned nparams, afb_data_t const *params){
-    fprintf(stderr, "Hello !\n");
+  fprintf(stderr, "Hello !\n");
+}
+
+static void call(afb_req_t request, unsigned nparams, afb_data_t const *params){
+  fprintf(stderr, "Hello : call !\n");
+  afb_data_array_addref(nparams, params);
+  afb_req_reply(request, 0, nparams, params);
+}
+
+static void subscribe(afb_req_t request, unsigned nparams, afb_data_t const *params){
+  int rc;
+  afb_api_new_event(afbBindingRoot, "event", &event);
+  rc = afb_req_subscribe(request, event);
+  if (rc >= 0){
+    fprintf(stderr, "Hello : subscribe success!\n");
     afb_data_array_addref(nparams, params);
     afb_req_reply(request, 0, nparams, params);
+  }
+  else
+    fprintf(stderr, "Hello : subscribe fail !\n");
+}
+
+static void unsubscribe(afb_req_t request, unsigned nparams, afb_data_t const *params){
+  int rc;
+  rc = afb_req_unsubscribe(request, event);
+  if(rc >= 0){
+    fprintf(stderr, "Hello : unsubscribe success !\n");  
+    afb_data_array_addref(nparams, params);
+    afb_req_reply(request, 0, nparams, params);
+  }
+  else fprintf(stderr, "Hello : unsubscribe fail !\n");
+}
+
+static void evpush(afb_req_t request, unsigned nparams, afb_data_t const *params){
+  int rc;
+  afb_data_array_addref(nparams, params);
+  rc = afb_event_push(event, nparams, params);
+  if( rc>= 0){
+    fprintf(stderr, "Hello : evpush sucess !\n");
+    afb_data_array_addref(nparams, params);
+    afb_req_reply(request, 0, nparams, params);
+  }
+  else fprintf(stderr, "Hello : evpush fail !\n");
 }
 
 static const struct afb_verb_v4 verbs[]= {
-  { .verb="hello",        .callback=hello }
+  { .verb="hello",        .callback=hello },
+  { .verb="call",         .callback=call },
+  { .verb="subscribe",    .callback=subscribe },
+  { .verb="unsubscribe",  .callback=unsubscribe },
+  { .verb="evpush",       .callback=evpush },
+  { .verb=NULL }
 };
 
+static int mainctl(afb_api_x4_t api, afb_ctlid_t ctlid, afb_ctlarg_t ctlarg, void *userdata){
+  switch(ctlid){
+    case afb_ctlid_Root_Entry :
+      fprintf(stderr, "Binding hello receved control signal %d : afb_ctlid_Root_Entry\n", afb_ctlid_Root_Entry);
+    break;
+    
+    case afb_ctlid_Pre_Init :
+      fprintf(stderr, "Binding hello receved control signal %d : afb_ctlid_Pre_Init\n", afb_ctlid_Pre_Init);
+    break;
+    
+    case afb_ctlid_Init :
+      fprintf(stderr, "Binding hello receved control signal %d : afb_ctlid_Init\n", afb_ctlid_Init);
+    break;
+    
+    case afb_ctlid_Class_Ready :
+      fprintf(stderr, "Binding hello receved control signal %d : afb_ctlid_Class_Ready\n", afb_ctlid_Class_Ready);
+    break;
+    
+    case afb_ctlid_Orphan_Event :
+      fprintf(stderr, "Binding hello receved control signal %d : afb_ctlid_Orphan_Event\n", afb_ctlid_Orphan_Event);
+    break;
+    
+    case afb_ctlid_Exiting :
+      fprintf(stderr, "Binding hello receved control signal %d : afb_ctlid_Exiting\n", afb_ctlid_Exiting);
+    break;
+  }
+  
+  return 0;
+}
 const struct afb_binding_v4 afbBindingExport = {
-	.api = APINAME,
-	.specification = NULL,
-	.verbs = verbs,
-	.mainctl = mainctl
+  .api = APINAME,
+  .specification = NULL,
+  .verbs = verbs,
+  .mainctl = mainctl
 };
