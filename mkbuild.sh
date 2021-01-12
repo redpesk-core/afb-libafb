e5df4a5220650eb7e61ec050f69fa5875737f68c
José Bollo <jose.bollo@iot.bzh>
2021-01-12 18:44:31 +0100

    Normalize mkbuild.sh

--- mkbuild.sh
+++ mkbuild.sh
@@ -2,13 +2,11 @@
 
 h="$(dirname $0)"
 force=false
-arch=
-bd=
-: ${PREFIX:=}
-eval set -- $(getopt -o a:b:fp: -l arch:,buildir:,force,prefix: -- "$@") || exit
+: ${bd:=build}
+: ${PREFIX:=$HOME/.local}
+eval set -- $(getopt -o b:fp: -l buildir:,force,prefix: -- "$@") || exit
 while :; do
 	case "$1" in
-	-a|--arch) arch="$2"; shift;;
 	-b|--buildir) bd="$2"; shift;;
 	-f|--force) force=true;;
 	-p|--prefix) PREFIX="$2"; shift;;
@@ -17,20 +15,13 @@ while :; do
 	shift
 done
 
-case "$arch" in
-  32) ARCH32=ON; : ${CMAKE_INSTALL_PREFIX:=$HOME/.local32};;
-  64) : ${CMAKE_INSTALL_PREFIX:=$HOME/.local64};;
-  *) arch=;;
-esac
-: ${bd:=build${arch}}
-
 mkdir -p "$h/$bd" || exit
 cd "$h/$bd" || exit
 
 $force && { rm -r * 2>/dev/null || rm CMakeCache.txt 2>/dev/null; }
 test -f CMakeCache.txt -a -f Makefile || \
 cmake \
-	-DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX:=${PREFIX:=$HOME/.local}} \
+	-DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX:=$PREFIX} \
 	-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE:=Debug} \
 	-DWITH_SYSTEMD=${WITH_SYSTEMD:=ON} \
 	-DWITHOUT_CYNAGORA=${WITHOUT_CYNAGORA:=OFF} \
