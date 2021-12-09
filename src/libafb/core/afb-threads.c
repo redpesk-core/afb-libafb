7b1da5946881e246ae89889e8e874198024bdcfb
José Bollo <jose.bollo@iot.bzh>
2021-12-09 18:11:12 +0100

    afb-threads: Add basic thread manager

---
+++ src/libafb/core/afb-threads.c
@@ -0,0 +1,398 @@
+/*
+ * Copyright (C) 2015-2021 IoT.bzh Company
+ * Author: José Bollo <jose.bollo@iot.bzh>
+ *
+ * $RP_BEGIN_LICENSE$
+ * Commercial License Usage
+ *  Licensees holding valid commercial IoT.bzh licenses may use this file in
+ *  accordance with the commercial license agreement provided with the
+ *  Software or, alternatively, in accordance with the terms contained in
+ *  a written agreement between you and The IoT.bzh Company. For licensing terms
+ *  and conditions see https://www.iot.bzh/terms-conditions. For further
+ *  information use the contact form at https://www.iot.bzh/contact.
+ *
+ * GNU General Public License Usage
+ *  Alternatively, this file may be used under the terms of the GNU General
+ *  Public license version 3. This license is as published by the Free Software
+ *  Foundation and appearing in the file LICENSE.GPLv3 included in the packaging
+ *  of this file. Please review the following information to ensure the GNU
+ *  General Public License requirements will be met
+ *  https://www.gnu.org/licenses/gpl-3.0.html.
+ * $RP_END_LICENSE$
+ */
+
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#include "sys/x-mutex.h"
+#include "sys/x-cond.h"
+#include "sys/x-thread.h"
+#include "sys/x-errno.h"
+
+#include "core/afb-sig-monitor.h"
+#include "core/afb-threads.h"
+#include "sys/verbose.h"
+
+#define DEBUGGING 0
+#if DEBUGGING
+#include <stdio.h>
+#define PRINT(...) fprintf(stderr,__VA_ARGS__)
+#else
+#define PRINT(...)
+#endif
+
+/** Description of threads */
+struct thread
+{
+	/** next thread of the list */
+	struct thread *next;
+
+	/** thread id */
+	x_thread_t tid;
+
+	/** stop requested */
+	volatile unsigned char active: 1;
+
+	/** is asleep */
+	volatile unsigned char asleep: 1;
+
+	/** class of the thread */
+	int classid;
+
+	/** job getter */
+	afb_threads_job_getter_t getjob;
+
+	/** closure of the job getter */
+	void *getjobcls;
+
+	/** synchronisation with the thread */
+	x_cond_t  cond;
+
+};
+
+/* synchronisation of threads */
+static x_mutex_t mutex = X_MUTEX_INITIALIZER;
+static x_cond_t *asleep_waiter_cond = 0;
+
+
+/* list of threads */
+static struct thread *threads = 0;
+
+static int active_count = 0;
+static int asleep_count = 0;
+
+static inline int match_any_class(int classid)
+{
+	return !~classid;
+}
+
+static inline int match_class(struct thread *thr, int classid)
+{
+	return thr->classid & classid;
+}
+
+static void link_thread(struct thread *thr)
+{
+	thr->next = threads;
+	threads = thr;
+	active_count++;
+}
+
+static void unlink_thread(struct thread *thr)
+{
+	struct thread *ithr;
+	if (threads == thr)
+		threads = thr->next;
+	else {
+		for (ithr = threads ; ithr && ithr->next != thr ; ithr = ithr->next);
+		if (ithr)
+			ithr->next = thr->next;
+	}
+}
+
+static inline void wakeup_asleep_waiter(x_cond_t *newcond)
+{
+	x_cond_t *oldcond = asleep_waiter_cond;
+	asleep_waiter_cond = newcond;
+	if (oldcond)
+		x_cond_signal(oldcond);
+}
+
+static inline void cancel_asleep_waiter(x_cond_t *ifcond)
+{
+	if (asleep_waiter_cond == ifcond)
+		asleep_waiter_cond = 0;
+}
+
+static int wakeup(struct thread *thr)
+{
+	if (!thr->asleep)
+		return 0;
+
+PRINT("++++++++++++ WUsB%p\n",thr);
+	thr->asleep = 0;
+	asleep_count--;
+	x_cond_signal(&thr->cond);
+PRINT("++++++++++++ WUsA%p\n",thr);
+	return 1;
+}
+
+static void stop(struct thread *thr)
+{
+	active_count--;
+	thr->active = 0;
+	wakeup_asleep_waiter(0);
+	wakeup(thr);
+}
+
+static void thread_run(struct thread *me)
+{
+	int status;
+	afb_threads_job_desc_t jobdesc;
+	x_thread_t tid = me->tid;
+
+PRINT("++++++++++++ START %p\n",me);
+	/* initiate thread tempo */
+	afb_sig_monitor_init_timeouts();
+
+	while (me->active) {
+		/* get a job */
+		x_mutex_unlock(&mutex);
+		status = me->getjob(me->getjobcls, &jobdesc, tid);
+		if (status > 0) {
+			/* run the job */
+			jobdesc.run(jobdesc.job, tid);
+			x_mutex_lock(&mutex);
+		}
+		else {
+			x_mutex_lock(&mutex);
+			if (me->active) {
+				if (status < 0)
+					stop(me);
+				else {
+					/* no job, wait */
+					me->asleep = 1;
+					asleep_count++;
+PRINT("++++++++++++ TRwB%p\n",me);
+					wakeup_asleep_waiter(0);
+					x_cond_wait(&me->cond, &mutex);
+PRINT("++++++++++++ TRwA%p\n",me);
+				}
+			}
+		}
+	}
+	/* terminate */
+	afb_sig_monitor_clean_timeouts();
+
+PRINT("++++++++++++ STOP %p\n",me);
+}
+
+static void thread_starter(void *thr)
+{
+	x_mutex_lock(&mutex);
+	thread_run(thr);
+	unlink_thread(thr);
+	x_mutex_unlock(&mutex);
+	free(thr);
+	x_thread_exit(0);
+}
+
+static int start_thread(struct thread *thr)
+{
+	int rc;
+
+	x_mutex_lock(&mutex);
+	link_thread(thr);
+	rc = x_thread_create(&thr->tid, thread_starter, thr, 1);
+	if (rc < 0)
+		unlink_thread(thr);
+	x_mutex_unlock(&mutex);
+	if (rc < 0) {
+		free(thr);
+		CRITICAL("not able to start thread: %s", strerror(-rc));
+	}
+	return rc;
+}
+
+static int enter_thread(struct thread *thr)
+{
+	thr->tid = x_thread_self();
+	x_mutex_lock(&mutex);
+	link_thread(thr);
+	thread_run(thr);
+	unlink_thread(thr);
+	x_mutex_unlock(&mutex);
+	free(thr);
+	return 0;
+}
+
+static int start(int classid, afb_threads_job_getter_t jobget, void * closure, int (*run)(struct thread *thr))
+{
+	int rc;
+	struct thread *thr = malloc(sizeof *thr);
+	if (!thr)
+		rc = -ENOMEM;
+	else {
+		thr->next = 0;
+		thr->tid = 0;
+		thr->active = 1;
+		thr->asleep = 0;
+		thr->cond = (x_cond_t)PTHREAD_COND_INITIALIZER;
+		thr->classid = classid;
+		thr->getjob = jobget;
+		thr->getjobcls = closure;
+		rc = run(thr);
+	}
+	return rc;
+}
+
+int afb_threads_active_count(int classid)
+{
+	struct thread *ithr;
+	int count;
+	x_mutex_lock(&mutex);
+	if (match_any_class(classid))
+		count = active_count;
+	else {
+		for (count = 0, ithr = threads ; ithr ; ithr = ithr->next)
+			count += (ithr->active && match_class(ithr, classid));
+	}
+	x_mutex_unlock(&mutex);
+	return count;
+}
+
+int afb_threads_asleep_count(int classid)
+{
+	struct thread *ithr;
+	int count;
+	x_mutex_lock(&mutex);
+	if (match_any_class(classid))
+		count = asleep_count;
+	else {
+		for (count = 0, ithr = threads ; ithr ; ithr = ithr->next)
+			count += (ithr->asleep && match_class(ithr, classid));
+	}
+	x_mutex_unlock(&mutex);
+	return count;
+}
+
+int afb_threads_start(int classid, afb_threads_job_getter_t jobget, void *closure)
+{
+	return start(classid, jobget, closure, start_thread);
+}
+
+int afb_threads_enter(int classid, afb_threads_job_getter_t jobget, void *closure)
+{
+	return start(classid, jobget, closure, enter_thread);
+}
+
+int afb_threads_wakeup(int classid, int count)
+{
+	int decount = 0;
+	struct thread *ithr;
+	x_mutex_lock(&mutex);
+	for (ithr = threads ; ithr && decount < count ; ithr = ithr->next)
+		if (ithr->active && match_class(ithr, classid))
+			decount += wakeup(ithr);
+	x_mutex_unlock(&mutex);
+	return decount;
+}
+
+int afb_threads_stop(int classid, int count)
+{
+	int decount = 0;
+	struct thread *ithr;
+	x_mutex_lock(&mutex);
+	for (ithr = threads ; ithr && decount < count ; ithr = ithr->next)
+		if (ithr->active && match_class(ithr, classid)) {
+			stop(ithr);
+			decount++;
+		}
+	x_mutex_unlock(&mutex);
+	return decount;
+}
+
+static struct thread *get_thread(x_thread_t tid)
+{
+	struct thread *ithr;
+	for (ithr = threads ; ithr && !x_thread_equal(ithr->tid, tid) ; ithr = ithr->next);
+	return ithr;
+}
+
+int afb_threads_has_thread(x_thread_t tid)
+{
+	int resu;
+	struct thread *thr;
+	x_mutex_lock(&mutex);
+	thr = get_thread(tid);
+	resu = thr ? thr->classid : 0;;
+	x_mutex_unlock(&mutex);
+	return resu;
+}
+
+int afb_threads_stop_thread(x_thread_t tid)
+{
+	int resu;
+	struct thread *thr;
+	x_mutex_lock(&mutex);
+	thr = get_thread(tid);
+	if ((resu = (thr && thr->active)))
+		stop(thr);
+	x_mutex_unlock(&mutex);
+	return resu;
+}
+
+int afb_threads_has_me()
+{
+	return afb_threads_has_thread(x_thread_self());
+}
+
+int afb_threads_stop_me()
+{
+	return afb_threads_stop_thread(x_thread_self());
+}
+
+int afb_threads_wait_idle(int classid, int timeoutms)
+{
+	x_thread_t tid = x_thread_self();
+	int rc, resu = 0;
+	struct timespec expire;
+	struct thread *ithr;
+	if (timeoutms > 0) {
+		clock_gettime(CLOCK_REALTIME, &expire);
+		expire.tv_sec += timeoutms / 1000;
+		expire.tv_nsec = (timeoutms % 1000) * 1000000;
+		if (expire.tv_nsec >= 1000000000) {
+			expire.tv_nsec -= 1000000000;
+			expire.tv_sec++;
+		}
+	}
+	x_mutex_lock(&mutex);
+	for (ithr = threads ; ithr ; ) {
+		if (!x_thread_equal(ithr->tid, tid) /* not me */
+		 && match_class(ithr, classid) /* match the given class */
+		 && ithr->active && !ithr->asleep /* active but not asleep */) {
+			x_cond_t cond = PTHREAD_COND_INITIALIZER;
+			wakeup_asleep_waiter(&cond);
+			if (timeoutms <= 0)
+				x_cond_wait(&cond, &mutex);
+			else {
+				rc = x_cond_timedwait(&cond, &mutex, &expire);
+				if (rc) {
+					if (errno == ETIMEDOUT) {
+						cancel_asleep_waiter(&cond);
+						resu = X_ETIMEDOUT;
+						break;
+					}
+				}
+			}
+			cancel_asleep_waiter(&cond);
+			ithr = threads;
+		}
+		else
+			ithr = ithr->next;
+	}
+	x_mutex_unlock(&mutex);
+	return resu;
+}
---
+++ src/libafb/core/afb-threads.h
@@ -0,0 +1,192 @@
+/*
+ * Copyright (C) 2015-2021 IoT.bzh Company
+ * Author: José Bollo <jose.bollo@iot.bzh>
+ *
+ * $RP_BEGIN_LICENSE$
+ * Commercial License Usage
+ *  Licensees holding valid commercial IoT.bzh licenses may use this file in
+ *  accordance with the commercial license agreement provided with the
+ *  Software or, alternatively, in accordance with the terms contained in
+ *  a written agreement between you and The IoT.bzh Company. For licensing terms
+ *  and conditions see https://www.iot.bzh/terms-conditions. For further
+ *  information use the contact form at https://www.iot.bzh/contact.
+ *
+ * GNU General Public License Usage
+ *  Alternatively, this file may be used under the terms of the GNU General
+ *  Public license version 3. This license is as published by the Free Software
+ *  Foundation and appearing in the file LICENSE.GPLv3 included in the packaging
+ *  of this file. Please review the following information to ensure the GNU
+ *  General Public License requirements will be met
+ *  https://www.gnu.org/licenses/gpl-3.0.html.
+ * $RP_END_LICENSE$
+ */
+
+#pragma once
+
+/**
+ * This module implement a basic thread manager.
+ * Each started (or entered -see below-) thread run
+ * the loop below:
+ *
+ *    while (running) {
+ *       sts = getjob(closure, &jobdesc, threadid);
+ *       if (sts > 0)
+ *            jobdesc.run(jobdesc.job, threadid);
+ *       else if (sts < 0)
+ *            running = 0;
+ *       else
+ *            sleep_until_awaken();
+ *    }
+ *
+ * The function for getting the job is given at creation
+ * of the thread.
+ */
+
+/**
+ * Each started thread has a classid. The classid is an
+ * integer seen for its bits.
+ *
+ * The constant AFB_THREAD_ANY_CLASS matches any classid
+ */
+#define AFB_THREAD_ANY_CLASS (-1)
+
+/**
+ * Structure for getting jobs.
+ *
+ * When getting a job, the structure must be filled with accurate values
+ * if the getter returns a positive number.
+ */
+struct afb_threads_job_desc
+{
+	/** the routine to call */
+	void (*run)(void *job, x_thread_t tid);
+
+	/** the first argument for 'run' */
+	void *job;
+};
+
+/**
+ * Alias for the structure
+ */
+typedef struct afb_threads_job_desc afb_threads_job_desc_t;
+
+/**
+ * The call back for getting the jobs.
+ *
+ * That function receives:
+ *  @param closure     a pointer given when creating the thread
+ *  @param desc        a pointer for describing the job to be run
+ *  @param tid         the thread id of the current thread
+ *
+ * It must return:
+ *  - a positive value for running the job set in the desciption
+ *  - a negative value for stopping the thread
+ *  - zero for waiting to be awaken
+ */
+typedef int (*afb_threads_job_getter_t)(void *closure, afb_threads_job_desc_t *desc, x_thread_t tid);
+
+/**
+ * start a thread of classid with a job getter
+ *
+ * @param classid classid of the started thread
+ * @param jobget  the getter function @see afb_threads_job_getter_t
+ * @param closure closure for the getter
+ *
+ * @return 0 on succes or a negative error code
+ */
+extern int afb_threads_start(int classid, afb_threads_job_getter_t jobget, void *closure);
+
+/**
+ * dont start a thread but use the current one to run the loop.
+ *
+ * @param classid classid of the started thread
+ * @param jobget  the getter function @see afb_threads_job_getter_t
+ * @param closure closure for the getter
+ *
+ * @return 0 on succes or a negative error code
+ */
+extern int afb_threads_enter(int classid, afb_threads_job_getter_t jobget, void *closure);
+
+/**
+ * Get the count of managed threads matching the classid mask and
+ * not stopped.
+ *
+ * @param classid the mask of the threads to count
+ *
+ * @return the count found
+ */
+extern int afb_threads_active_count(int classid);
+
+/**
+ * Get the count of managed threads matching the classid mask and
+ * being sleeping.
+ *
+ * @param classid the mask of the threads to count
+ *
+ * @return the count found
+ */
+extern int afb_threads_asleep_count(int classid);
+
+/**
+ * Wake up the managed threads matching the classid mask and
+ * being sleeping.
+ *
+ * @param classid the mask of the threads to wakeup
+ * @param count   the maximum count of thread to awake
+ *
+ * @return the count of thread awaken
+ */
+extern int afb_threads_wakeup(int classid, int count);
+
+/**
+ * Wake up the managed threads matching the classid mask.
+ *
+ * @param classid the mask of the threads to wakeup
+ * @param count   the maximum count of thread to stop
+ *
+ * @return the count of thread stopped
+ */
+extern int afb_threads_stop(int classid, int count);
+
+/**
+ * Checks if the given thread is managed
+ *
+ * @param tid  the thread id of the thread to check
+ *
+ * @return 1 if the thread is managed or 0 else
+ */
+extern int afb_threads_has_thread(x_thread_t tid);
+
+/**
+ * Stop the given thread if it is managed
+ *
+ * @param tid  the thread id of the thread to stop
+ *
+ * @return 1 if the thread is stopped or 0 else
+ */
+extern int afb_threads_stop_thread(x_thread_t tid);
+
+/**
+ * Checks if the current thread is managed
+ *
+ * @return 1 if the current thread is managed or 0 else
+ */
+extern int afb_threads_has_me();
+
+/**
+ * Stop the current thread if it is managed
+ *
+ * @return 1 if the thread is stopped or 0 else
+ */
+extern int afb_threads_stop_me();
+
+/**
+ * Wait until all managed threads matching the classid mask are sleeping.
+ * Correctly handles the case of the current thread being managed.
+ *
+ * @param classid the mask of the threads to wait
+ * @param timeout if greater than zero, the timeout in milliseconds to wait.
+ *
+ * @return 0 on success or a negative error code on timeout
+ */
+extern int afb_threads_wait_idle(int classid, int timeoutms);
