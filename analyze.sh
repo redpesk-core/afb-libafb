b7fa7a175392c9618c562e9e89ea88a65c3caf77
José Bollo <jose.bollo@iot.bzh>
2023-02-20 17:16:31 +0100

    Improve analyze tools

--- analyze.sh
+++ analyze.sh
@@ -10,7 +10,17 @@ doit() {
 	echo
 	figlet clang 2>/dev/null || echo C L A N G
 	echo
-	clang --analyze --analyzer-output text -I src/libafb -I build/src/libafb src/libafb/*/*.c
+	clang-14 --analyze --analyzer-output text -I src/libafb -I build/src/libafb -I ~/.locenv/afb/include src/libafb/*/*.c
+
+	echo
+	figlet gcc 2>/dev/null || echo G C C
+	echo
+	gcc -fanalyzer -I src/libafb -I build/src/libafb -I ~/.locenv/afb/include src/libafb/*/*.c
+
+	echo
+	figlet clang-tidy 2>/dev/null || echo C L A N G - T I D Y
+	echo
+	clang-tidy -p build/compile_commands.json -config-file .clang-tidy  src/libafb/*/*.c
 }
 
 doit 2>&1 | tee analyze.output
